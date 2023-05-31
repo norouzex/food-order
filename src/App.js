259e5eff20810ac2f73a18b0059a79e5097cb9ad
norouzy <mohamadnewday@gmail.com>
2023-05-31 14:57:59 +0330

    get data from api and add loading

--- src/App.js
+++ src/App.js
@@ -5,13 +5,67 @@ import Cart from "./components/Cart/Cart";
 import CartProvider from "./Store/CartProvider";
 
 function App() {
+  // async function addMelasHandler(meal) {
+  //   const response = await fetch(
+  //     "https://parseapi.back4app.com/classes/meals",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(meal),
+  //       headers: {
+  //         "X-Parse-Application-Id": "honFhwdYWhAtSbsJ4TjNkeu6hs6NUPrHXBb9mXwL",
+  //         "X-Parse-REST-API-Key": "P8RZhjsW4oDzngOE3p7jUl5LRhwqyuV9brFg8euu",
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
     setCartIsShown(!cartIsShown);
   };
+  // const test = () => {
+  //   const DUMMY_MEALS = [
+  //     {
+  //       // id: "m1",
+  //       name: "Sushi",
+  //       info: "Finest fish and veggies",
+  //       price: 22.99,
+  //     },
+  //     {
+  //       // id: "m2",
+  //       name: "Schnitzel",
+  //       info: "A german specialty!",
+  //       price: 16.5,
+  //     },
+  //     {
+  //       // id: "m3",
+  //       name: "Barbecue Burger",
+  //       info: "American, raw, meaty",
+  //       price: 12.99,
+  //     },
+  //     {
+  //       // id: "m4",
+  //       name: "Green Bowl",
+  //       info: "Healthy...and green...",
+  //       price: 18.99,
+  //     },
+  //   ];
+
+  //   for (const i in DUMMY_MEALS) addMelasHandler(DUMMY_MEALS[i]);
+  // };
+
   return (
     <CartProvider>
+      {/* <button
+        style={{ zIndex: "999", backgroundColor: "blue", position: "absolute" }}
+        onClick={test}
+      >
+        submitted
+      </button> */}
       {cartIsShown && <Cart onClose={showCartHandler} />}
       <MainHeader showCarts={showCartHandler} />
       <Meal />
--- src/components/Meal/MealsList.js
+++ src/components/Meal/MealsList.js
@@ -1,41 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./MealsList.module.css";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card/Card";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    info: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    info: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    info: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    info: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     info: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     info: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     info: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     info: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const MealsList = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://parseapi.back4app.com/classes/meals",
+        {
+          method: "GET",
+          headers: {
+            "X-Parse-Application-Id":
+              "honFhwdYWhAtSbsJ4TjNkeu6hs6NUPrHXBb9mXwL",
+            "X-Parse-REST-API-Key": "P8RZhjsW4oDzngOE3p7jUl5LRhwqyuV9brFg8euu",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData["results"]) {
+        loadedMeals.push({
+          id: responseData["results"][key].objectId,
+          name: responseData["results"][key].name,
+          info: responseData["results"][key].info,
+          price: responseData["results"][key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []);
+  if (isLoading) {
+    return (
+      <section className={style.MealsLoading}>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
   return (
     <section className={style.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((meal) => (
+          {meals.map((meal) => (
             <MealItem
               key={meal.id}
               id={meal.id}
